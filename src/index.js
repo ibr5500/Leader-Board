80d2e7a8140307f1ca2be3e4c0f4b373336720c4
unknown <ibr.s.ahmat@gmail.com>
2022-05-18 15:06:06 +0300

    Fix linters errors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,1 @@
 import './style.css';
-
-
-console.log(111);
--- webpack.config.js
+++ webpack.config.js
@@ -2,43 +2,43 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: path.resolve(__dirname, './src/index.js'),
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-        clean: true,
+  mode: 'development',
+  entry: path.resolve(__dirname, './src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true,
+  },
+  devtool: 'source-map',
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
     },
-    devtool: 'source-map',
-    devServer: {
-        static: {
-            directory: path.resolve(__dirname, 'dist'),
+    port: 5555,
+    open: true,
+  },
+  module: {
+    rules: [{
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader'],
+    },
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
         },
-        port: 5555,
-        open: true,
+      },
     },
-    module: {
-        rules: [{
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env'],
-                    },
-                },
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Leader Board',
-            filename: 'index.html',
-            template: './src/template.html',
-        }),
     ],
-}
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Leader Board',
+      filename: 'index.html',
+      template: './src/template.html',
+    }),
+  ],
+};
