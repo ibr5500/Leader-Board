f38a93bef3cad45ea659b522c284fb9ea7d6593f
unknown <ibr.s.ahmat@gmail.com>
2022-05-19 13:58:05 +0300

    Create usersScore module that contain userScore class

---
+++ src/modules/usersScore.js
@@ -0,0 +1,53 @@
+class UserScore {
+    constructor(user, score) {
+        this.user = user;
+        this.score = score;
+    }
+
+    playersData = [];
+
+    url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/QE8nBkXmdfd1YoxrlAjT/scores/';
+
+    getData = () => {
+
+        const scores = document.getElementById('scores-container');
+        scores.innerHTML = this.playersData.map((elem, index) => `<p class=${index % 2 !== 0? 'row-bg' : ''} >${elem.user}: <span>${elem.score}</span></p>`).join('');
+
+    }
+
+    fetchData = async() => {
+        try {
+            const data = await fetch(this.url);
+            const response = await data.json();
+
+            response.result.map((elem) => this.playersData.push(elem));
+            return this.getData();
+
+        } catch (error) {
+            return error;
+        }
+    };
+
+    addNewScore = async({ user, score }) => {
+        try {
+            const record = {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ user, score: score }),
+            };
+
+            const data = await fetch(this.url, record);
+            const response = await data.json();
+            this.scoreData.push(response);
+            return this.fetchData();
+
+        } catch (error) {
+            return error;
+        }
+    }
+}
+
+export default UserScore;
