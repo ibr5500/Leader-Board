e8e07e562796abbbf3e84d39d6d8be6a2f6c6a9e
unknown <ibr.s.ahmat@gmail.com>
2022-05-19 14:00:40 +0300

    Create eventlistener for the form to submit user data to the UserScore class

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import './style.css';
+import UserScore from './modules/usersScore';
+
+const newUserScore = new UserScore();
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const userName = form.name.value;
+    const userScore = form.score.value;
+    newUserScore.addNewScore({ userName, userScore });
+    form.name.value = '';
+    form.score.value = '';
+});
+
+const refersh = document.getElementById('refresh');
+refersh.addEventListener('click', newUserScore.fetchData);
+window.addEventListener('DOMContentLoaded', newUserScore.getData);
