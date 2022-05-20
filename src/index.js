f70549257b012b17bc2acbafc74c128a4fcbc13a
unknown <ibr.s.ahmat@gmail.com>
2022-05-20 15:38:12 +0300

    Make the scores appear when reloading the page

--- src/index.js
+++ src/index.js
@@ -5,19 +5,24 @@ const newUserScore = new UserScore();
 const form = document.querySelector('.form');
 
 form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  const user = form.name.value;
-  const score = form.score.value;
-  newUserScore.addNewScore({ user, score });
-  form.name.value = '';
-  form.score.value = '';
+    event.preventDefault();
+    const user = form.name.value;
+    const score = form.score.value;
+    newUserScore.addNewScore({ user, score });
+    form.name.value = '';
+    form.score.value = '';
 });
 
 const scoresList = () => {
-  newUserScore.playersData = [];
-  newUserScore.fetchData();
+    newUserScore.playersData = [];
+    newUserScore.fetchData();
 };
+
+const displayOnLoad = () => {
+    newUserScore.fetchData();
+    newUserScore.getData();
+}
 
 const refersh = document.getElementById('refresh');
 refersh.addEventListener('click', scoresList);
-window.addEventListener('DOMContentLoaded', newUserScore.getData);
+window.addEventListener('load', displayOnLoad);
