d71a4b79b81db13d111145e6bb826be0bc37784d
unknown <ibr.s.ahmat@gmail.com>
2022-05-20 15:43:18 +0300

    Fix linters errors

--- src/index.js
+++ src/index.js
@@ -5,23 +5,23 @@ const newUserScore = new UserScore();
 const form = document.querySelector('.form');
 
 form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const user = form.name.value;
-    const score = form.score.value;
-    newUserScore.addNewScore({ user, score });
-    form.name.value = '';
-    form.score.value = '';
+  event.preventDefault();
+  const user = form.name.value;
+  const score = form.score.value;
+  newUserScore.addNewScore({ user, score });
+  form.name.value = '';
+  form.score.value = '';
 });
 
 const scoresList = () => {
-    newUserScore.playersData = [];
-    newUserScore.fetchData();
+  newUserScore.playersData = [];
+  newUserScore.fetchData();
 };
 
 const displayOnLoad = () => {
-    newUserScore.fetchData();
-    newUserScore.getData();
-}
+  newUserScore.fetchData();
+  newUserScore.getData();
+};
 
 const refersh = document.getElementById('refresh');
 refersh.addEventListener('click', scoresList);
